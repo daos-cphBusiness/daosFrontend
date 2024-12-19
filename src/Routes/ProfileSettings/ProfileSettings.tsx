3a4824aae55ef3612f15933e50873d71f27cac58
Sabin Bhandari <95691066+meIsLost@users.noreply.github.com>
2024-12-19 20:31:22 +0100

    deleteUser done

--- src/Routes/ProfileSettings/ProfileSettings.tsx
+++ src/Routes/ProfileSettings/ProfileSettings.tsx
@@ -1,9 +1,47 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button/Button";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./ProfileSettings.module.css";
 
 export function ProfileSettings() {
+  const navigate = useNavigate();
+
+  async function handleOnDelete(): Promise<void> {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete your profile?"
+    );
+    if (confirmed) {
+      try {
+        const token = localStorage.getItem("authToken"); // Retrieve JWT from local storage
+        if (!token) {
+          alert("User not authenticated");
+          throw new Error("User not authenticated");
+        }
+        const response = await fetch("/api/users", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          console.log("Profile deleted successfully.");
+          alert("Profile deleted successfully.");
+          navigate("/sign-in");
+        } else {
+          console.error("Failed to delete profile:", response.statusText);
+          alert("Failed to delete profile. Please try again.");
+        }
+      } catch (error) {
+        console.error("Error occurred while deleting profile:", error);
+        alert("An error occurred. Please try again.");
+      }
+    } else {
+      console.log("Delete action canceled.");
+    }
+  }
+
   return (
     <>
       <Navbar />
@@ -17,7 +55,9 @@ export function ProfileSettings() {
               </Link>
             </div>
             <div>
-              <Button variant="delete">Delete Profile</Button>
+              <Button variant="delete" onClick={handleOnDelete}>
+                Delete Profile
+              </Button>
             </div>
           </div>
         </div>
